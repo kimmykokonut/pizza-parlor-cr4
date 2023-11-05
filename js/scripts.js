7a1a39f86f8f574ac24e50e4ca6cb0894fc907e8
Kim Robinson <kimmykokonut@gmail.com>
2023-11-05 15:19:23 -0800

    update styling

--- js/scripts.js
+++ js/scripts.js
@@ -26,17 +26,14 @@ const sizePriceObject = {
 
 function handleFormSubmission(e) {
   e.preventDefault();
-
   const nameInput = document.querySelector("input#nameInput").value;
   const userSizeRadioSelection = document.querySelector("input[name='size']:checked").value;
   const userToppingSelections = document.querySelectorAll("input[name=toppings]:checked");
-
   const receiptDiv = document.querySelector("div#receipt");
   const h3Name = document.createElement("h3");
   h3Name.append("Name: " + nameInput);
   const pSize = document.createElement("p");
   pSize.append("Size: " + userSizeRadioSelection);
-
   const ulTopping = document.createElement("ul");
   const userToppingsSelectArray = Array.from(userToppingSelections);
   userToppingsSelectArray.forEach(function (element) {
@@ -45,14 +42,12 @@ function handleFormSubmission(e) {
     ulTopping.append(liTopping);
   });
   const pizzaOrder = new Pizza(userToppingsSelectArray, userSizeRadioSelection);
-
   const h4NewOrderCost = document.createElement("h4");
   const newOrderCost = pizzaOrder.getTotalCost();
   h4NewOrderCost.append("Grand Total: " + "$" + newOrderCost);
   receiptDiv.append(h3Name, pSize, ulTopping, h4NewOrderCost);
   receiptDiv.removeAttribute("class");
 }
-
 
 window.addEventListener("load", function () {
   this.document.querySelector("form#orderForm").addEventListener("submit", handleFormSubmission);
